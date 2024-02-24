e1691d81b72b2e5412f564cd9b6b9ca177417b73
R Kumawat <108892371+rohanpin1@users.noreply.github.com>
2024-02-24 14:42:56 +0530

    Initial commit

---
+++ App Code/Suggestion.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Cms.Core.IO;
+
+namespace Umbraco.Cms.Core.PropertyEditors
+{
+	[DataEditor(
+		alias: "Suggestions editor",
+		name: "Suggestions",
+		view: "~/App_Plugins/Suggestions/suggestion.html",
+		Group = "Common",
+		Icon = "icon-list")]
+	public class Suggestions : DataEditor
+	{
+		public Suggestions(IDataValueEditorFactory dataValueEditorFactory)
+			: base(dataValueEditorFactory)
+		{
+		}
+	}
+}
---
+++ App_Plugins/Suggestions/suggestion.controller.js
@@ -0,0 +1,16 @@
+﻿angular.module("umbraco")
+    .controller("SuggestionPluginController",
+        // Scope object is the main object which is used to pass information from the controller to the view.
+        function ($scope) {
+
+            // SuggestionPluginController assigns the suggestions list to the aSuggestions property of the scope
+            $scope.aSuggestions = ["Discover New Paths", "with truly integrated logistics", "How about starting a book club today or this week?", "A new path has been found. Where it leads is for you to discover.","Access the latest articles, trends and research to guide your strategy and keep you updated on what's new in the supply chain world."];
+
+            // The controller assigns the behavior to scope as defined by the getSuggestion method, which is invoked when the user clicks on the 'Give me Suggestions!' button.
+            $scope.getSuggestion = function () {
+
+                // The getSuggestion method reads a random value from an array and provides a Suggestion. 
+                $scope.model.value = $scope.aSuggestions[$scope.aSuggestions.length * Math.random() | 0];
+            }
+
+        });
